306992159baacc4993e81268b3b580ee0df90599
Volta <giovoltadesign@gmail.com>
2021-05-06 21:18:43 -0400

    components: Loader Error

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/components/Error.sccs";
+import ErrorImage from "../assets/static/images/error.png";
+
+const Error = () => (
+	<div className="erro">
+		<img src={ErrorImage} alt="Morty Scared" />
+		<span>Sorry, an error has occurred</span>
+	</div>
+);
+
+export default Error;
