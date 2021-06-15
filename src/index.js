6eee9d9efa42dd1be12cd370b712f8409c6c2f75
Volta <giovoltadesign@gmail.com>
2021-06-15 09:12:48 -0400

    redux: babel - webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import App from "./routes/App";
 
 import { store } from "./store";
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	etry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "public"),
+		__fillname: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.svg$/,
+				use: ["@svgr/webpack"],
+			},
+			{
+				test: /\.(gif|png|jpg?g)$/i,
+				use: [
+					"file-loader",
+					{
+						loader: "image-webpack-loader",
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ["style-loader", "sccs-loader", "sass-loader"],
+			},
+		],
+		resolve: {
+			extension: [".js", ".jsx"],
+		},
+		performance: {
+			hints: "error",
+			maxEntrypointSize: 580000,
+			maxAssetSize: 580000,
+		},
+
+		// devtool: "eval-cheap-module-source-map",
+		devtool: "source-map",
+
+		devServer: {
+			contentBase: path(__dirname, "public"),
+		},
+	},
+};
